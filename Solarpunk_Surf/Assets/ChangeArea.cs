9283f32fbe0112f4016f7b1848bfa528cfc6a2f8
SteeRevo <69092521+SteeRevo@users.noreply.github.com>
2023-03-17 04:17:50 -0700

    added ranking system

--- Solarpunk_Surf/Assets/ChangeArea.cs
+++ Solarpunk_Surf/Assets/ChangeArea.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ChangeArea : MonoBehaviour
 {
@@ -11,11 +12,24 @@ public class ChangeArea : MonoBehaviour
 
     public Animator transitionAnim;
 
+    public GameObject rankObject;
+
+    public TMP_Text rankText;
+
+    public TMP_Text timeText;   
+
+    public TMP_Text healthText;
+
+    public TMP_Text currentTimer;
+    
+    private PlayerState playerSt;
+
     private PlayerController playerScript;
     // Start is called before the first frame update
     void Start()
     {
         playerScript = Player.GetComponent<PlayerController>();
+        playerSt     = Player.GetComponent<PlayerState>();
     }
 
     // Update is called once per frame
@@ -29,14 +43,41 @@ public class ChangeArea : MonoBehaviour
         Debug.Log(other.gameObject.tag);
         if(other.gameObject.tag == "Player" && playerScript.checkItem())
         {
+            rankObject.SetActive(true);
+            float timeCount = float.Parse(currentTimer.text);
+            Debug.Log(timeCount);
+            timeText.text = "Time: " + currentTimer.text;
+            healthText.text = "Health: " + playerSt.Health.ToString();
+            if(timeCount < 15 && playerSt.Health > 4)
+            {
+                rankText.text = "Rank: S";
+            }
+            else if (timeCount < 20 && playerSt.Health > 3)
+            {
+                rankText.text = "Rank: A";
+            }
+            else if (timeCount < 25 && playerSt.Health > 2)
+            {
+                rankText.text = "Rank: B";
+            }
+            else if (timeCount < 30 && playerSt.Health > 1)
+            {
+                rankText.text = "Rank: C";
+            }
+            else {
+                rankText.text = "Rank: D";
+            }
+            playerScript.OnDisable();
             StartCoroutine(LoadScene());
+             
         }
     }
 
     IEnumerator LoadScene()
     {
+        
         transitionAnim.SetTrigger("end");
-        yield return new WaitForSeconds(2.1f);
+        yield return new WaitForSeconds(7.1f);
         SceneManager.LoadScene(levelIndex);
     }
    
--- Solarpunk_Surf/Assets/ChangeAreaNormal.cs
+++ Solarpunk_Surf/Assets/ChangeAreaNormal.cs
@@ -2,14 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ChangeAreaNormal : MonoBehaviour
 {
     public int levelIndex;
 
-    private PlayerController playerScript;
+    public PlayerController playerScript;
 
     public Animator transitionAnim;
+
+    public GameObject rankObject;
+
+    public TMP_Text rankText;
+
+    public TMP_Text timeText;   
+
+    public TMP_Text healthText;
+
+    public TMP_Text currentTimer;
+    
+    public PlayerState playerSt;
+
+    public GameObject turrets;
+
+    [SerializeField]
+    int SRankTime, SRankHealth, ARankTime, ARankHealth, 
+    BRankTime, BRankHealth, CRankTime, CRankHealth; 
+
     // Start is called before the first frame update
 
     // Update is called once per frame
@@ -23,6 +43,32 @@ public class ChangeAreaNormal : MonoBehaviour
         Debug.Log(other.gameObject.tag);
         if(other.gameObject.tag == "Player")
         {
+            turrets.SetActive(false);
+            rankObject.SetActive(true);
+            float timeCount = float.Parse(currentTimer.text);
+            Debug.Log(timeCount);
+            timeText.text = "Time: " + currentTimer.text;
+            healthText.text = "Health: " + playerSt.Health.ToString();
+            if(timeCount < SRankTime && playerSt.Health > SRankHealth)
+            {
+                rankText.text = "Rank: S";
+            }
+            else if (timeCount < ARankTime && playerSt.Health > ARankHealth)
+            {
+                rankText.text = "Rank: A";
+            }
+            else if (timeCount < BRankTime && playerSt.Health > BRankHealth)
+            {
+                rankText.text = "Rank: B";
+            }
+            else if (timeCount < CRankTime && playerSt.Health > CRankHealth)
+            {
+                rankText.text = "Rank: C";
+            }
+            else {
+                rankText.text = "Rank: D";
+            }
+            playerScript.OnDisable();
             StartCoroutine(LoadScene());
         }
     }
@@ -30,7 +76,7 @@ public class ChangeAreaNormal : MonoBehaviour
     IEnumerator LoadScene()
     {
         transitionAnim.SetTrigger("end");
-        yield return new WaitForSeconds(2.1f);
+        yield return new WaitForSeconds(7.0f);
         SceneManager.LoadScene(levelIndex);
     }
 }
